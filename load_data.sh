6a3e54bc64e366a4c8b48d1cf3ee2d73b984e13e
Alexander Yusov <yusov.aa@phystech.edu>
2021-10-28 22:36:17 +0300

    Add load_data script

---
+++ load_data.sh
@@ -0,0 +1,9 @@
+data_dir=$1
+db_name=${2:-"lake"}
+
+for path in $(ls $data_dir/*.csv)
+do
+    filename=$(python -c "print('$path'.split('.csv')[0].split('/')[-1])")
+    echo "Inserting into table $filename given path $path"
+    csvsql --db postgresql://admin:admin@localhost:5432/$db_name --tables $filename --insert $path
+done
