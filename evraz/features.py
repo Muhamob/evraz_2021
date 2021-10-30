289b15635a6d7f4f2424c2f5f299281471ef6c4b
Alexander Yusov <yusov.aa@phystech.edu>
2021-10-30 21:38:50 +0300

    Add temperature features

--- evraz/features.py
+++ evraz/features.py
@@ -18,7 +18,7 @@ class DBFeatureExtractor(TransformerMixin, BaseEstimator):
         self.feature_columns = None
         self.int_columns = None
 
-    def _get_df(self, mode: str, cond: str = "") -> pd.DataFrame:
+    def get_df(self, mode: str, cond: str = "") -> pd.DataFrame:
         if mode == 'train':
             target = 'target_train'
         elif mode == 'test':
@@ -40,12 +40,14 @@ class DBFeatureExtractor(TransformerMixin, BaseEstimator):
         Происходит инференс типов данных
         """
 
-        df = self._get_df("train", cond="limit 500")
+        df = self.get_df("train", cond="limit 500")
 
-        self.time_columns = df.columns[df.dtypes == 'datetime64[ns]'].difference(self.target_columns + [self.id_column]).tolist()
-        self.float_columns = df.columns[df.dtypes == 'float64'].difference(self.target_columns + [self.id_column]).tolist()
-        self.int_columns = df.columns[df.dtypes == 'int64'].difference(self.target_columns + [self.id_column]).tolist()
-        self.cat_columns = df.columns[df.dtypes == 'object'].difference(self.target_columns + [self.id_column]).tolist()
+        self.time_columns = df.columns[df.dtypes == 'datetime64[ns]'].difference(
+            self.target_columns + [self.id_column, 'tgt_NPLV']).tolist()
+        self.float_columns = df.columns[df.dtypes == 'float64'].difference(
+            self.target_columns + [self.id_column, 'tgt_NPLV']).tolist()
+        self.int_columns = df.columns[df.dtypes == 'int64'].difference(self.target_columns + [self.id_column, 'tgt_NPLV']).tolist()
+        self.cat_columns = df.columns[df.dtypes == 'object'].difference(self.target_columns + [self.id_column, 'tgt_NPLV']).tolist()
 
         self.feature_columns = self.float_columns + self.int_columns + self.cat_columns
         print("Feature columns", self.feature_columns)
@@ -53,14 +55,69 @@ class DBFeatureExtractor(TransformerMixin, BaseEstimator):
         return self
 
     def transform(self, X=None, mode: str = 'train'):
-        df = self._get_df(mode).astype({n: pd.CategoricalDtype() for n in self.cat_columns})
-
-        return df if mode == 'test' else df.dropna(subset=self.target_columns)
+        df = self.get_df(mode).astype({n: pd.CategoricalDtype() for n in self.cat_columns})
+        return df
+        # return df if mode == 'test' else df.dropna(subset=self.target_columns)
 
 
 class AllFeaturesExtractor(DBFeatureExtractor):
     query_template = """
-    with chronom_features as (
+    select *
+    from {target}
+    where "NPLV" != 511135
+    order by "NPLV"
+    """
+
+    def __init__(self, conn):
+        super().__init__(conn)
+
+        self.feature_extractors = dict(
+            static_fe=StaticFeatures(conn),
+            chronom_fe=ChronomRawFeatures(conn),
+            sip_fe=SipFeatures(conn),
+            gas_fe=GasRawFeatures(conn),
+            gas_proc_fe=GasProcessFeatures(conn)
+        )
+
+    def fit(self, X=None, y=None, **kwargs):
+        self.feature_columns = []
+
+        for name, extractor in self.feature_extractors.items():
+            print(f"Fit {name} feature extractor")
+            extractor.fit()
+            self.feature_columns.extend(extractor.feature_columns)
+        return self
+
+    def transform(self, X=None, mode: str = 'train'):
+        target = self.get_df(mode)
+        if mode == "train":
+            target = target.dropna(subset=["TST", "C"])
+
+        for name, extractor in self.feature_extractors.items():
+            print(f"Extracting features from {name} extractor")
+            features = extractor.transform(mode=mode)
+            print(features)
+            target = pd.merge(target, features, how='left', on='NPLV')
+
+        return target
+
+
+class StaticFeatures(DBFeatureExtractor):
+    query_template = """
+    select target."NPLV" "tgt_NPLV",
+           plavki.*,
+           chugun.*
+    from {target} target
+          left join plavki_{mode} plavki using ("NPLV")
+          left join chugun_{mode} chugun using ("NPLV")
+    """
+
+    def transform(self, X=None, mode: str = 'train'):
+        return super().transform(X, mode).drop(columns=["NPLV"]).rename(columns={'tgt_NPLV': 'NPLV'})
+
+
+class ChronomRawFeatures(DBFeatureExtractor):
+    query_template = """
     select target."NPLV",
            sum(coalesce(chronom."O2", 0))                                   "sum_O2",
            coalesce(sum(datediff_minutes(chronom."VR_KON", chronom."VR_NACH"))
@@ -81,107 +138,127 @@ class AllFeaturesExtractor(DBFeatureExtractor):
     from {target} target
              left join chronom_{mode} chronom using ("NPLV")
     group by target."NPLV"
-),
-     static_features as (
-         select *
-         from {target} target
-                  left join plavki_{mode} plavki using ("NPLV")
-                  left join chugun_{mode} chugun using ("NPLV")
-), grouped_features as (
-    select "NPLV",
-           -- кол-во, сумма, среднее, отклонение, скошенность засыпанного Уголь ТО
-           count("VSSYP") filter (where "NMSYP" = 'Уголь ТО') ugol_cnt,
-           coalesce(sum("VSSYP") filter (where "NMSYP" = 'Уголь ТО'), 0) ugol_sum,
-           coalesce(avg("VSSYP") filter (where "NMSYP" = 'Уголь ТО'), 0) ugol_avg,
-           coalesce(stddev("VSSYP") filter (where "NMSYP" = 'Уголь ТО'), 0) ugol_std,
-           -- кол-во, сумма, среднее, отклонение, скошенность засыпанного ФЛЮМАГ
-           count("VSSYP") filter (where "NMSYP" = 'ФЛЮМАГ') flumag_cnt,
-           coalesce(sum("VSSYP") filter (where "NMSYP" = 'ФЛЮМАГ'), 0) flumag_sum,
-           coalesce(avg("VSSYP") filter (where "NMSYP" = 'ФЛЮМАГ'), 0) flumag_avg,
-           coalesce(stddev("VSSYP") filter (where "NMSYP" = 'ФЛЮМАГ'), 0) flumag_std,
-           -- кол-во, сумма, среднее, отклонение, скошенность засыпанного изв_ЦОИ
-           count("VSSYP") filter (where "NMSYP" = 'изв_ЦОИ') uzvcoi_cnt,
-           coalesce(sum("VSSYP") filter (where "NMSYP" = 'изв_ЦОИ'), 0) uzvcoi_sum,
-           coalesce(avg("VSSYP") filter (where "NMSYP" = 'изв_ЦОИ'), 0) uzvcoi_avg,
-           coalesce(stddev("VSSYP") filter (where "NMSYP" = 'изв_ЦОИ'), 0) uzvcoi_std,
-           -- кол-во, сумма, среднее, отклонение, скошенность засыпанного Флюс ФОМИ
-           count("VSSYP") filter (where "NMSYP" = 'Флюс ФОМИ') flusfomi_cnt,
-           coalesce(sum("VSSYP") filter (where "NMSYP" = 'Флюс ФОМИ'), 0) flusfomi_sum,
-           coalesce(avg("VSSYP") filter (where "NMSYP" = 'Флюс ФОМИ'), 0) flusfomi_avg,
-           coalesce(stddev("VSSYP") filter (where "NMSYP" = 'Флюс ФОМИ'), 0) flusfomi_std
-    from sip_{mode} sip
-    -- from sip_test sip
-    group by "NPLV"
-), numbered as (
--- проставление порядкого номера процессов внутри одной плавки
-    select *,
-           row_number() over (partition by "NPLV" order by "DAT_OTD") rank,
-           row_number() over (partition by "NPLV" order by "DAT_OTD" desc) rank_inv
-    from sip_{mode} sip
-), sip_features as (
--- объединение признаков сыпучих
-    select gp.*,
-           -- первое название сыпучих
-           first_n."NMSYP" first_sip,
-           -- последнее название
-           last_n."NMSYP" last_sip
-    from grouped_features gp
-    left join numbered first_n using("NPLV")
-    left join numbered last_n using("NPLV")
-    where 1=1
-    -- первая строка
-    and first_n.rank = 1
-    -- последняя
-    and last_n.rank_inv = 1
-), gas_features as (
+    """
+
+
+class SipFeatures(DBFeatureExtractor):
+    query_template = """
+    with grouped_features as (
+        select "NPLV",
+               -- кол-во, сумма, среднее, отклонение, скошенность засыпанного Уголь ТО
+               count("VSSYP") filter (where "NMSYP" = 'Уголь ТО') ugol_cnt,
+               coalesce(sum("VSSYP") filter (where "NMSYP" = 'Уголь ТО'), 0) ugol_sum,
+               coalesce(avg("VSSYP") filter (where "NMSYP" = 'Уголь ТО'), 0) ugol_avg,
+               coalesce(stddev("VSSYP") filter (where "NMSYP" = 'Уголь ТО'), 0) ugol_std,
+               -- кол-во, сумма, среднее, отклонение, скошенность засыпанного ФЛЮМАГ
+               count("VSSYP") filter (where "NMSYP" = 'ФЛЮМАГ') flumag_cnt,
+               coalesce(sum("VSSYP") filter (where "NMSYP" = 'ФЛЮМАГ'), 0) flumag_sum,
+               coalesce(avg("VSSYP") filter (where "NMSYP" = 'ФЛЮМАГ'), 0) flumag_avg,
+               coalesce(stddev("VSSYP") filter (where "NMSYP" = 'ФЛЮМАГ'), 0) flumag_std,
+               -- кол-во, сумма, среднее, отклонение, скошенность засыпанного изв_ЦОИ
+               count("VSSYP") filter (where "NMSYP" = 'изв_ЦОИ') uzvcoi_cnt,
+               coalesce(sum("VSSYP") filter (where "NMSYP" = 'изв_ЦОИ'), 0) uzvcoi_sum,
+               coalesce(avg("VSSYP") filter (where "NMSYP" = 'изв_ЦОИ'), 0) uzvcoi_avg,
+               coalesce(stddev("VSSYP") filter (where "NMSYP" = 'изв_ЦОИ'), 0) uzvcoi_std,
+               -- кол-во, сумма, среднее, отклонение, скошенность засыпанного Флюс ФОМИ
+               count("VSSYP") filter (where "NMSYP" = 'Флюс ФОМИ') flusfomi_cnt,
+               coalesce(sum("VSSYP") filter (where "NMSYP" = 'Флюс ФОМИ'), 0) flusfomi_sum,
+               coalesce(avg("VSSYP") filter (where "NMSYP" = 'Флюс ФОМИ'), 0) flusfomi_avg,
+               coalesce(stddev("VSSYP") filter (where "NMSYP" = 'Флюс ФОМИ'), 0) flusfomi_std
+        from sip_{mode} sip
+        -- from sip_test sip
+        group by "NPLV"
+    ), numbered as (
+    -- проставление порядкого номера процессов внутри одной плавки
+        select *,
+               row_number() over (partition by "NPLV" order by "DAT_OTD") rank,
+               row_number() over (partition by "NPLV" order by "DAT_OTD" desc) rank_inv
+        from sip_{mode} sip
+    ), sip_features as (
+    -- объединение признаков сыпучих
+        select gp.*,
+               -- первое название сыпучих
+               first_n."NMSYP" first_sip,
+               -- последнее название
+               last_n."NMSYP" last_sip
+        from grouped_features gp
+        left join numbered first_n using("NPLV")
+        left join numbered last_n using("NPLV")
+        where 1=1
+        -- первая строка
+        and first_n.rank = 1
+        -- последняя
+        and last_n.rank_inv = 1
+    )
+    select * from sip_features
+    {cond}
+    """
+
+
+class GasRawFeatures(DBFeatureExtractor):
+    query_template = """
     select "NPLV",
            -- мин, среднее, макс, откл
            -- объём
-           -- min("V") min_V,
-           -- max("V") max_V,
+           min("V") min_V,
+           max("V") max_V,
            avg("V") avg_V,
            stddev("V") srd_V,
            -- температура
-           -- min("T") min_T,
-           -- max("T") max_T,
+           min("T") min_T,
+           max("T") max_T,
            avg("T") avg_T,
            stddev("T") srd_T,
            -- AR
-           -- min("AR") min_AR,
-           -- max("AR") max_AR,
+           min("AR") min_AR,
+           max("AR") max_AR,
            avg("AR") avg_AR,
            stddev("AR") srd_AR,
            -- CO
-           -- min("CO") min_CO,
-           -- max("CO") max_CO,
+           min("CO") min_CO,
+           max("CO") max_CO,
            avg("CO") avg_CO,
            stddev("CO") srd_CO,
            -- CO2
-           -- min("CO2") min_CO2,
-           -- max("CO2") max_CO2,
+           min("CO2") min_CO2,
+           max("CO2") max_CO2,
            avg("CO2") avg_CO2,
            stddev("CO2") srd_CO2,
            -- H2
-           -- min("H2") min_H2,
-           -- max("H2") max_H2,
+           min("H2") min_H2,
+           max("H2") max_H2,
            avg("H2") avg_H2,
            stddev("H2") srd_H2,
            -- O2
-           -- min("O2") min_O2,
-           -- max("O2") max_O2,
+           min("O2") min_O2,
+           max("O2") max_O2,
            avg("O2") avg_O2,
            stddev("O2") srd_O2,
            -- N2
-           -- min("N2") min_N2,
-           -- max("N2") max_N2,
+           min("N2") min_N2,
+           max("N2") max_N2,
            avg("N2") avg_N2,
            stddev("N2") srd_N2
-        from gas_{mode} gas
-        group by "NPLV"
-)
-select * from static_features
-left join chronom_features using("NPLV")
-left join sip_features using("NPLV")
-left join gas_features using("NPLV")
-where static_features."NPLV" != 511135
-"""
+    from gas_{mode} gas
+    group by "NPLV"
+    {cond}
+    """
+
+
+class GasProcessFeatures(DBFeatureExtractor):
+    query_template = """
+    select gas."NPLV",
+       avg(gas."T") filter ( where chronom."NOP" = 'Продувка' ) avg_t_produvka,
+       stddev(gas."T") filter ( where chronom."NOP" = 'Продувка' ) std_t_produvka,
+       min(gas."T") filter ( where chronom."NOP" = 'Продувка' ) min_t_produvka,
+       percentile_cont(0.1) within group ( order by gas."T" ) filter ( where chronom."NOP" = 'Продувка' ) percentile_10_produvka,
+       percentile_cont(0.9) within group ( order by gas."T" ) filter ( where chronom."NOP" = 'Продувка' ) percentile_90_produvka
+    -- from gas_train gas
+    -- left join chronom_train chronom
+    from gas_{mode} gas
+    left join chronom_{mode} chronom
+    on gas."NPLV" = chronom."NPLV"
+    and gas."Time" between chronom."VR_NACH" and chronom."VR_KON"
+    group by 1
+    {cond}
+    """
