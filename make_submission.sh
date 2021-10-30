d9f1b4293383ba02397ede6f714750dc9acfda8b
Alexander Yusov <yusov.aa@phystech.edu>
2021-10-31 01:01:57 +0300

    Stop if training failed

--- make_submission.sh
+++ make_submission.sh
@@ -1,5 +1,5 @@
 submission_name=$(date +"%Y-%m-%d_%H-%M-%S")
 echo $submission_name
-PYTHONPATH=. SUB_NAME=$submission_name python make_submission.py | tee ../data/logs/$submission_name.log
+PYTHONPATH=. SUB_NAME=$submission_name python make_submission.py | tee ../data/logs/$submission_name.log || exit 1
 git archive -v --format=zip HEAD > ../data/submissions/$submission_name.zip
 #git tag -a $submission_name -m "$submission_name"
